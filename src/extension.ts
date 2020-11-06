21e6bcb9d327bf4d04cefa14efb30db6f1e10ed7
Osama Abdul Rehman <osama.a.rehman@gmail.com>
2020-11-06 18:12:02 +0500

    Adds simple protractor test runner extension command

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,132 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const languages = ["typescript", "javascript"];
+	const window = vscode.window;
+	const workspace = vscode.workspace;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "run-debug-protractor" is now active!');
+	const isTestRegex = /^((\s*)(it|describe)(\s*)\((\s*)('|"))/g;
+	const isTestSetRegex = /^((\s*)(describe)(\s*)\((\s*)('|"))/g;
+	const testNameRegex = /(("|')(.*?)("|'))/;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('run-debug-protractor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	function provideCodeLenses(
+		document: vscode.TextDocument,
+		token: vscode.CancellationToken
+	) {
+		if (!window || !window.activeTextEditor) return;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from run-debug-protractor!');
+		let matches: Match[] = [];
+		let activeDocument = window.activeTextEditor.document;
+
+		for (let index = 0; index < activeDocument.lineCount; index++) {
+			const lineText = activeDocument.lineAt(index).text;
+
+			if (isTestRegex.test(lineText)) {
+				const testNameMatch = lineText.match(testNameRegex);
+
+				const match: Match = {
+					range: new vscode.Range(
+						new vscode.Position(index, 0),
+						new vscode.Position(index, 5)
+					),
+					testName: testNameMatch ? testNameMatch[3] : "",
+					testFile: activeDocument.fileName,
+					isTestSet: isTestSetRegex.test(lineText),
+				};
+
+				matches.push(match);
+			}
+		}
+
+		return matches.map(
+			(match) =>
+				new vscode.CodeLens(match.range, {
+					title: match.isTestSet ? "Run tests" : "Run test",
+					command: "extension.executeProtractorTest",
+					arguments: [match],
+				})
+		);
+	}
+
+	function getProtractorConfigPath() {
+		const configuration = workspace.getConfiguration(
+			"protractor-conf-file-path"
+		);
+
+		let protactorConfigPath = "";
+		if (
+			workspace &&
+			workspace.rootPath &&
+			configuration &&
+			configuration.protractorConfiguration
+		) {
+			protactorConfigPath = path.join(
+				workspace.rootPath,
+				configuration.protractorConfiguration
+			);
+		}
+
+		return protactorConfigPath;
+	}
+
+	let disposable = vscode.commands.registerCommand(
+		"extension.executeProtractorTest",
+		(match) => {
+			const terminal = window.createTerminal();
+			terminal.show();
+
+			const protactorConfigPath = getProtractorConfigPath();
+			const testFile = match.testFile;
+			const testName = match.testName;
+
+			// const wsedit = new vscode.WorkspaceEdit();
+
+			// const wsPath = workspace.rootPath;
+			// const filePath = vscode.Uri.file(wsPath + "/.vscode/launch.json");
+			// vscode.window.showInformationMessage(filePath.toString());
+
+			// wsedit.createFile(filePath, { ignoreIfExists: true });
+
+			// vscode.workspace.applyEdit(wsedit);
+			// vscode.window.showInformationMessage("Created a new file: launch.json");
+
+			terminal.sendText(`cd "${workspace.rootPath}"`);
+			terminal.sendText(
+				`node node_modules/protractor/bin/protractor ${protactorConfigPath} --specs='${testFile}' --grep="${testName}"`
+			);
+		}
+	);
+
+	// let helloWorldDisposable = vscode.commands.registerCommand(
+	// 	"run-debug-protractor.helloWorld",
+	// 	() => {
+	// 		vscode.window.showInformationMessage(
+	// 			"Hello from my Protractor Extension"
+	// 		);
+	// 	}
+	// );
+
+	languages.forEach((language) => {
+		context.subscriptions.push(
+			vscode.languages.registerCodeLensProvider(language, { provideCodeLenses })
+		);
 	});
 
 	context.subscriptions.push(disposable);
+	// context.subscriptions.push(helloWorldDisposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+export interface Match {
+	range: vscode.Range;
+	testName: string;
+	testFile: string;
+	isTestSet: boolean;
+}
