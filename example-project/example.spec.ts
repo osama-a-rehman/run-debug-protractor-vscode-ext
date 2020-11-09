33a7e2766c3bbc2f43ce4312ad21c73129e10fff
Osama Abdul Rehman <osama.a.rehman@gmail.com>
2020-11-09 16:54:56 +0500

    Updates example.spec.ts

--- example-project/example.spec.ts
+++ example-project/example.spec.ts
@@ -1,24 +1,20 @@
 import { launch, Page } from "puppeteer";
 
-describe("Puppeteer Test", function () {
-	it("should open google.com and wait for 5 seconds", async () => {
-		try {
-			const browser = await launch({
-				headless: false,
-			});
+describe("Protractor Test", function () {
+	it("should open 'Google' in browser", async () => {
+		const browser = await launch({
+			headless: false,
+		});
 
-			const page: Page = await browser.newPage();
-			await page.setViewport({
-				width: 1440,
-				height: 900,
-				deviceScaleFactor: 1,
-			});
+		const page: Page = await browser.newPage();
+		await page.setViewport({
+			width: 1440,
+			height: 900,
+			deviceScaleFactor: 1,
+		});
 
-			await page.goto("https://www.google.com.pk");
-			await page.waitForTimeout(5000);
-		} catch (error) {
-			console.log(error);
-		}
+		await page.goto("https://www.google.com.pk");
+		await page.waitForTimeout(5000);
 
 		console.log("Ran Test 1");
 	});
